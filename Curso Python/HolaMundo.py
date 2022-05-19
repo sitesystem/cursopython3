c5277f858dab4ece2c905f08def16951a1442e9f
sitesystem <l_aloaxa05@hotmail.com>
2022-05-18 19:50:55 -0500

    Primera subida de archivos

---
+++ Curso Python/HolaMundo.py
@@ -0,0 +1,11 @@
+# COMENTARIO: Saludo primer programa
+# Los comentarios nos sirven para documentar y describir
+# cada parte del programa
+# Los comentario no los toma en cuenta o los omite Python
+
+print("Hola Mundo")
+print('Eduardo González');
+print(5+5)
+print(10-2)
+print(8*3)
+print(20/3)
